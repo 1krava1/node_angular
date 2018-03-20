2a90a6be43f5689c9ee46b25adfd7377e0bc7fb6
nick <n.kravchenko@skins.services>
2018-03-20 18:11:38 +0200

    Add several components for view only

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './signup/signup.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
+    component: MainComponent,
   },
   {
     path: 'signup',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 import { UserService } from './shared/services/user.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,11 +18,15 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     SignUpComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
   ],
   imports: [
     HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    NgbModule.forRoot(),
     BrowserModule.withServerTransition({ appId: 'science' })
   ],
   providers: [
