92f375f7df5f602689801f61ff9ac3ffdf8e7dbe
nick <n.kravchenko@skins.services>
2018-04-03 10:46:33 +0300

    checkout form

--- src/app/shared/components/checkout/checkout.component.ts
+++ src/app/shared/components/checkout/checkout.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit, OnChanges {
   @Input() sum = 0;
 
   checkoutForm: FormGroup;
@@ -15,15 +15,16 @@ export class CheckoutComponent implements OnInit {
   paymentSystems = [
     {
       tag: 'bitcoin', name: 'bitcoin',
-      extraField: { name: 'walletNumber', title: 'Wallet Number' } },
+      extraField: { name: 'walletNumber', title: 'BTC Address' } },
     {
       tag: 'privat24', name: 'privat24',
-      extraField: { name: 'walletNumber', title: 'Wallet Number' } },
+      extraField: { name: 'walletNumber', title: 'Card Number' } },
     {
       tag: 'visa', name: 'visa',
-      extraField: { name: 'walletNumber', title: 'Wallet Number' } },
+      extraField: { name: 'walletNumber', title: 'Card Number' } },
     {
-      tag: 'paypal', name: 'paypal', },
+      tag: 'paypal', name: 'paypal',
+      extraField: { name: '', title: '' } },
     {
       tag: 'webmoney', name: 'webmoney',
       extraField: { name: 'walletNumber', title: 'Wallet Number' } },
@@ -35,7 +36,7 @@ export class CheckoutComponent implements OnInit {
       extraField: { name: 'walletNumber', title: 'Wallet Number' } },
     {
       tag: 'cell', name: 'cell',
-      extraField: { name: 'walletNumber', title: 'Wallet Number' } },
+      extraField: { name: 'walletNumber', title: 'Phone Number' } },
     {
       tag: 'pm', name: 'perfectmoney',
       extraField: { name: 'walletNumber', title: 'Wallet Number' } },
@@ -52,12 +53,34 @@ export class CheckoutComponent implements OnInit {
   ngOnInit() {
     this.createCheckoutForm();
   }
+  ngOnChanges(): void {
+    if ( typeof this.checkoutForm !== 'undefined' && this.sum === 0 ) {
+      this.checkoutForm.get('paymentSystem').setValue('');
+      console.log(this.checkoutForm.value);
+    }
+  }
 
   createCheckoutForm(): void {
     this.checkoutForm = this.formBuilder.group({
       paymentSystem: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
     });
+  }
+
+  extraField(): void {
+    if ( this.checkoutForm.get('paymentSystem').value === 'paypal' ) {
+      this.extraFieldTitle = '';
+      this.checkoutForm.removeControl('walletNumber');
+    } else {
+      this.extraFieldTitle = this.paymentSystems.filter( (ps) => {
+        if ( !!ps.extraField.title ) {
+          return ps.tag === this.checkoutForm.controls.paymentSystem.value;
+        } else {
+          return false;
+        }
+      })[0].extraField.title;
+      this.checkoutForm.addControl('walletNumber', new FormControl('', [Validators.required]));
+    }
   }
 
 }
